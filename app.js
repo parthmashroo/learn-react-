0fac54cb3cdd9a6c694a1f94f1c9ca5a2dac89a1
Parth Mashroo <pmashroo58@gmail.com>
2023-10-23 00:43:49 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  {
-    id: "parent",
-  },
-  [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "This is me learning React 🚀"),
-      React.createElement("h2", {}, "I'm an H2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an H1 tag"),
-      React.createElement("h2", {}, "I'm an H2 tag"),
-    ]),
-  ]
-);
-
+// React Create Element Method
 const heading = React.createElement(
   "h1",
-  {
-    id: "heading",
-    xyz: "abc",
-  },
-  "Hello World from React!!"
+  { id: "heading" },
+  "Namaste React Learning 🚀"
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const HeadingUsingJSX = () => (
+  <h1 id="heading" tabIndex="5" className="myclass">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+// React Component
+// Component Composition => A component inside a component is called Component Composition
+const HeadingComponent = () => (
+  <div className="container">
+    <HeadingUsingJSX />
+    {HeadingUsingJSX()}
+    {
+    /* You can also write JS inside JSX using { curly braces } 
+    this is very powerfull as you can wirte any JS and will get executed over here 
+    and JSX automatically stops CORS (cross site scripting attack) when it gets transpiled
+    */}
+    <HeadingUsingJSX></HeadingUsingJSX>
+    {/* This is one another way of calling component insisde component */}
+    <h1 id="heading" tabIndex="5" className="myclass">
+      This is React Functional Component
+    </h1>
+  </div>
+);
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
+/*
+            ☝☝☝☝ 
+            This is how you call a component to render usirng ReactDom 
+*/
